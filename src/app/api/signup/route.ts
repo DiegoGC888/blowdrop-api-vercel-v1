e8739fe711b7e9f4d9e502caf6ea0ad3192b34c7
Diego GC <diego.gcnx3@gmail.com>
2025-08-06 19:56:36 -0600

    feat nuevo endpoint signup agregado

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,48 @@
+// app/api/reset-pwd/route.ts
+export const runtime = 'edge';
+
+import { NextRequest, NextResponse } from "next/server";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*", // ⚠️ En producción, cambia por tu dominio
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+// Maneja el preflight (CORS OPTIONS)
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
+}
+
+export async function POST(req: NextRequest) {
+    console.log("Entro 1");
+
+  try {
+    const { email, password } = await req.json();
+console.log("SUPABASE:", process.env.SUPABASE_ANON_KEY);
+
+    const response = await fetch("https://cjsxbayumsbpvezswsfs.supabase.co/functions/v1/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Solo el backend conoce esta key, segura
+        Authorization: `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+      },
+      body: JSON.stringify({ email, password }),
+    });
+console.log("termino consumo de api");
+    const data = await response.json();
+    return new NextResponse(JSON.stringify(data), {
+      status: response.status,
+      headers: corsHeaders,
+    });
+  } catch (error) {
+    return new NextResponse(JSON.stringify({ error: "Error interno del servidor." }), {
+      status: 500,
+      headers: corsHeaders,
+    });
+  }
+}
